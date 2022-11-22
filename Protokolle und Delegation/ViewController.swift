efecf59e25b258193e2d9e941511751abdfe8fba
MichaHornung <michaelhornung2022@icloud.com>
2022-11-22 10:25:26 +0100

    Initial Commit

---
+++ Protokolle und Delegation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Protokolle und Delegation
+//
+//  Created by Michael Hornung on 22.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
